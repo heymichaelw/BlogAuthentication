05a3663592c226d07349d6b2c12c3b62619876ed
heymichaelw <heymichaelw@gmail.com>
2017-02-10 12:36:29 -0500

    Fixed layout

--- BlogAuthentication/App_Start/BundleConfig.cs
+++ BlogAuthentication/App_Start/BundleConfig.cs
@@ -11,18 +11,22 @@ namespace BlogAuthentication
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jqBootstrapValidation.js",
                         "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/clean-blog.js",
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                       "~/Scripts/bootstrap.js",
+                       "~/Scripts/clean-blog.js",
+                       "~/Scripts/clean-blog.min.js",
+                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css"));
+                      "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
+                      "~/Content/clean-blog.css"));
         }
     }
 }
