f8add57fe97bd34bcb4efb7c2bdd9e0cca30db52
Michael <Michael@DESKTOP-JF27GE5>
2017-02-10 11:00:27 -0500

    Add project files.

---
+++ BlogAuthentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogAuthentication.Startup))]
+namespace BlogAuthentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
